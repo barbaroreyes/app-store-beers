9a4f065fc5412aca211e9cc4e562b7b4acbd0a14
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-02 20:29:57 -0500

    update-card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,8 +2,17 @@ import React from 'react'
 import {Card,Button} from 'react-bootstrap'
 const CardId = (props) => {
   return (
-    <div >
-  <Card className='card' style={{ width: '18rem' }}>
+    <div  className ='card'>
+      <div>
+      <img src={props.image_url} alt=''/>
+      <p>{props.name}</p>
+       
+       <p>{props.tagline}</p>
+      </div>
+
+        
+
+  {/* <Card className='card' style={{ width: '18rem' }}>
   <Card.Title>{props.name}</Card.Title>
   <Card.Img style={{width:'50px'}} variant="top" src={props.image_url}/>
   <Card.Body>
@@ -13,7 +22,7 @@ const CardId = (props) => {
     </Card.Text>
     <Button variant="primary">Go somewhere</Button>
   </Card.Body>
-</Card>
+</Card> */}
     </div>
   )
 }
