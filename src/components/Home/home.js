106f987f62ab4c92ec3fb904c6f461e3046fa32a
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-03 15:04:31 -0500

    added- styles to home-page

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -6,6 +6,17 @@ const Home = () => {
       <div className="header">
         <div className="logo-box"></div>
       </div>
+       <div className="box">
+       <h1 className="primary">
+        <h1 className>
+          <span className="text1">
+            <p>Beer is one of the oldest</p>
+          </span>
+          <span className="text2">
+          and most widely consumed alcoholic drinks in the world
+          </span>
+        </h1></h1>
+       </div>
     </div>
   )
 }
