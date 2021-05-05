7ce959a30f2bfec3416d5e24b9b4c24e6ab84dc4
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-05 14:16:12 -0500

    update-cart-info

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -3,13 +3,14 @@ import './index.css'
 import {Link} from 'react-router-dom'
 
 
+
 const Nav = ({cart}) => {
   
   return (
     <div className='nav'>
        <Link to='/'>Home</Link>
        <Link to='/beers'>Beers</Link>
-       <Link to='/cart'>Cart : {cart.length}</Link>
+       <Link to='/cart'>{!cart.length? 'Cart': cart.length}</Link>
        <Link to='/namelist'>Register</Link>
        
     </div>
