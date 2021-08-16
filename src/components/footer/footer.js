ad413d892cca72a2596fb59a42d79cb2cab0746b
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-08-15 23:37:04 -0500

    clear-code

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,42 +1,14 @@
 import './index.css';
 
-import React, {
-  useEffect,
-  useState,
-} from 'react';
+import React from 'react';
 
 const Footer = () => {
-const [team, setTeam ]= useState([])
- const url = 'https://jsonplaceholder.typicode.com/users';
- useEffect(()=>{
-   (async ()=> {
-    const response = await fetch(url)
-    const data = await response.json()
-    setTeam(data)
-   })();
- },[])
-
-
-
   return (
     <div className='footer'>
-        {/* {team.splice(0,3).map((item,i)=>{
-             return (<div 
-              key={i} 
-              className='circle'>
-              {item.name}
-              </div>)
-         })} */}
+       
       <div className='rectangule'>
         <h3>Team</h3>
         <h1>Barbaro Reyes</h1>
-        
-         {/* {team.map((item,i)=>{
-           return(<p 
-              key={i}>
-              {item.name}
-              </p>)
-         })} */}
          </div>
          
     </div>
